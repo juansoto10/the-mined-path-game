63f9a1419aaaf683301ca39f5db22cfe169f6c4a
juansoto10 <jpsoto121@gmail.com>
2022-08-24 01:09:08 -0500

    Added player movement

--- js/game.js
+++ js/game.js
@@ -1,8 +1,18 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
+
 let canvasSize;
 let elementsSize;
+
+const playerPos = {
+    x: undefined,
+    y: undefined,
+};
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -12,7 +22,7 @@ function setCanvasSize() {
         canvasSize = window.innerWidth * 0.8;
     } else {
         canvasSize = window.innerHeight * 0.8;
-    }
+    };
 
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
@@ -23,9 +33,6 @@ function setCanvasSize() {
 }
 
 function startGame() {
-    // game.fillStyle = 'purple';
-    // game.fillText('Juan', 125, 50);
-
     console.log({ canvasSize, elementsSize })
 
     game.font = elementsSize + 'px Verdana';
@@ -35,21 +42,62 @@ function startGame() {
     const mapRows = map.trim().split('\n');
     const mapRowsCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowsCols});
-    // console.log(mapRowsCols[2][0]);
 
     mapRowsCols.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
             const emoji = emojis[col];
             const posX = elementsSize * (colIndex + 1);
             const posY = elementsSize * (rowIndex + 1);
-            game.fillText(emoji, posX, posY);
+
             console.log({ row, rowIndex, col, colIndex, emoji});
-        })
+
+            if (col == 'O') {
+                playerPos.x = posX;
+                playerPos.y = posY;
+                console.log({playerPos});
+            };
+
+            game.fillText(emoji, posX + 7, posY - 7);
+        });
     });
 
-    // for (let row = 1; row <= 10; row++) {
-    //     for (let col = 1; col <= 10; col++) {
-    //         game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * row + 7, elementsSize * col - 7);
-    //     }
-    // }
+    movePlayer();
 }
+
+function movePlayer() {
+    game.fillText(emojis['PLAYER'], playerPos.x + 7, playerPos.y - 7);
+};
+
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event) {
+    console.log(event);
+    if (event.key == 'ArrowUp') moveUp();
+    else if (event.key == 'ArrowLeft') moveLeft();
+    else if (event.key == 'ArrowRight') moveRight();
+    else if (event.key == 'ArrowDown') moveDown();
+};
+function moveUp() {
+    console.log('Up pressed');
+    playerPos.y -= elementsSize;
+    movePlayer();
+};
+function moveLeft() {
+    console.log('Left pressed');
+    playerPos.x -= elementsSize;
+    movePlayer();
+};
+function moveRight() {
+    console.log('Right pressed');
+    playerPos.x += elementsSize;
+    movePlayer();
+};
+function moveDown() {
+    console.log('Up pressed');
+    playerPos.y += elementsSize;
+    movePlayer();
+};
