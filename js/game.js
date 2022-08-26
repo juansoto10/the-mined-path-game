272b2e92ef5b2ef2117e3c6dd79c5dff5b6aaddc
juansoto10 <jpsoto121@gmail.com>
2022-08-25 23:37:51 -0500

    Added movement limits

--- js/game.js
+++ js/game.js
@@ -9,29 +9,50 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 
+// - Player position and gift position object -
 const playerPos = {
     x: undefined,
     y: undefined,
-};
+}
 
+const giftPos = {
+    x: undefined,
+    y: undefined,
+}
+
+// - Events when loading and resizing occurs -
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
+// - Truncate numbers -
+function trunc(num, positions) {
+    let numberToString = num.toString();
+    let stringLength = numberToString.length;
+    let decimalLength = numberToString.indexOf('.') + 1;
+    let subString = numberToString.substring(0, decimalLength + positions)
+    return Number(subString);
+}
+
+// - Setting canvas size -
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
+        initialSize = window.innerWidth * 0.8;
+        canvasSize = trunc(initialSize, 4)
     } else {
-        canvasSize = window.innerHeight * 0.8;
-    };
+        initialSize = window.innerHeight * 0.8;
+        canvasSize = trunc(initialSize, 4)
+    }
 
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    elementsSize = canvasSize / 10;
+    initialElementsSize = canvasSize / 10;
+    elementsSize = trunc(initialElementsSize, 4);
 
     startGame();
 }
 
+// - Rendering the map and the player after each move -
 function startGame() {
     console.log({ canvasSize, elementsSize })
 
@@ -41,63 +62,108 @@ function startGame() {
     const map = maps[0];
     const mapRows = map.trim().split('\n');
     const mapRowsCols = mapRows.map(row => row.trim().split(''));
-    console.log({map, mapRows, mapRowsCols});
+    // console.log({map, mapRows, mapRowsCols});
 
+    game.clearRect(0, 0, canvasSize, canvasSize);
     mapRowsCols.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
             const emoji = emojis[col];
-            const posX = elementsSize * (colIndex + 1);
-            const posY = elementsSize * (rowIndex + 1);
+            const posX = trunc(elementsSize * (colIndex + 1), 4);
+            const posY = trunc(elementsSize * (rowIndex + 1), 4);
 
-            console.log({ row, rowIndex, col, colIndex, emoji});
+            // console.log({ row, rowIndex, col, colIndex, emoji});
 
             if (col == 'O') {
-                playerPos.x = posX;
-                playerPos.y = posY;
-                console.log({playerPos});
-            };
+                if (!playerPos.x && !playerPos.y) {
+                    playerPos.x = posX;
+                    playerPos.y = posY;
+                    console.log({playerPos});
+                }
+            } else if (col == 'I') {
+                giftPos.x = posX;
+                giftPos.y = posY;
+                console.log({giftPos});
+            }
 
-            game.fillText(emoji, posX + 7, posY - 7);
+            game.fillText(emoji, posX, posY);
         });
     });
 
     movePlayer();
 }
 
+// - Render player position -
 function movePlayer() {
-    game.fillText(emojis['PLAYER'], playerPos.x + 7, playerPos.y - 7);
-};
+    // let truncPlayerPosX = trunc(playerPos.x, 4);
+    // let truncPlayerPosY = trunc(playerPos.y, 4);
+    // let truncGiftPosX = trunc(giftPos.x, 4);
+    // let truncGiftPosY = trunc(giftPos.y, 4);
+    // console.log({truncPlayerPosX, truncPlayerPosY, truncGiftPosX, truncGiftPosY});
 
+    const giftCollisionX = trunc(playerPos.x, 4) < trunc(giftPos.x, 4) + 0.1 && trunc(playerPos.y, 4) > trunc(giftPos.y, 4) - 0.1;
+    const giftCollisionY = trunc(playerPos.y, 4) < trunc(giftPos.y, 4) + 0.1 && trunc(playerPos.y, 4) > trunc(giftPos.y, 4) - 0.1;
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+    if (giftCollision) {
+        console.log('Level up Gonorrea!');
+    } 
+
+    console.log(`x = ${playerPos.x}, y = ${playerPos.y}`)
+    game.fillText(emojis['PLAYER'], trunc(playerPos.x, 4), trunc(playerPos.y, 4)); 
+}
+
+// - Events -
 window.addEventListener('keydown', moveByKeys);
 btnUp.addEventListener('click', moveUp);
 btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
 btnDown.addEventListener('click', moveDown);
 
+// - Player movement -
 function moveByKeys(event) {
     console.log(event);
     if (event.key == 'ArrowUp') moveUp();
     else if (event.key == 'ArrowLeft') moveLeft();
     else if (event.key == 'ArrowRight') moveRight();
     else if (event.key == 'ArrowDown') moveDown();
-};
+}
 function moveUp() {
     console.log('Up pressed');
-    playerPos.y -= elementsSize;
-    movePlayer();
-};
+
+    if ((playerPos.y - elementsSize) < elementsSize - 0.01) {
+        console.log('OUT');
+    } else {
+        playerPos.y = trunc(playerPos.y - elementsSize, 4);
+        startGame();
+    }
+}
 function moveLeft() {
     console.log('Left pressed');
-    playerPos.x -= elementsSize;
-    movePlayer();
-};
+    
+    if ((playerPos.x - elementsSize) < elementsSize - 0.01) {
+        console.log('OUT');
+    } else {
+        playerPos.x = trunc(playerPos.x - elementsSize, 4);
+        startGame();
+    }
+}
 function moveRight() {
     console.log('Right pressed');
-    playerPos.x += elementsSize;
-    movePlayer();
-};
+    
+    if ((playerPos.x + elementsSize) > canvasSize) {
+        console.log('OUT')
+    } else {
+        playerPos.x = trunc(playerPos.x + elementsSize, 4);
+        startGame();
+    }
+}
 function moveDown() {
-    console.log('Up pressed');
-    playerPos.y += elementsSize;
-    movePlayer();
-};
+    console.log('Down pressed');
+
+    if ((playerPos.y + elementsSize) > canvasSize) {
+        console.log('OUT');
+    } else {
+        playerPos.y = trunc(playerPos.y + elementsSize, 4);
+        startGame();
+    }  
+}
